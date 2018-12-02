54ce9df954221d043f07c2e41d19ef79c654378f
Rigel Lashley <Rigellashley@gmail.com>
2018-12-02 18:45:35 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,5 @@
-function() {
-  var XHR = new XMLHttpRequest();
+
+    var XHR = new XMLHttpRequest();
     XHR.open("GET", "https://evening-earth-96767.herokuapp.com/");
     XHR.send();
     console.log("sent");
-}();
